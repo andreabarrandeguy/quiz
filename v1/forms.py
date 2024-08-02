6f0d34844fd945f4ce0029b46652945ffba2d6ab
Andrea Barrandeguy <abarrandeguy87@hotmail.com>
2024-08-02 00:15:03 -0300

    Front end work done on index and create views

--- v1/forms.py
+++ v1/forms.py
@@ -4,8 +4,12 @@ from django import forms
 from v1.models import TemporaryQuestion, Room
 
 class NewRoomForm(forms.ModelForm):
-    user_name=forms.CharField(max_length=255, label=False, widget=forms.TextInput({"placeholder":"Your name"}))
-    other_person_name=forms.CharField(max_length=255, label=False, widget=forms.TextInput({"placeholder":"Receiver"}))
+    user_name=forms.CharField(max_length=255, label=False, widget=forms.TextInput(attrs={
+        'placeholder':'Your name',
+        'class': 'inputNames_field'}))
+    other_person_name=forms.CharField(max_length=255, label=False, widget=forms.TextInput(attrs={
+        'placeholder':'Who will receive this?',
+        'class': 'inputNames_field'}))
 
     class Meta:
         model=Room
@@ -13,7 +17,17 @@ class NewRoomForm(forms.ModelForm):
 
 
 class NewQuestionForm(forms.ModelForm):
-    question=forms.CharField(label=False, required=False, max_length=255, widget=forms.TextInput({"placeholder":"e.g. A weakeness"}))
+    question = forms.CharField(
+        label=False,
+        required=False,
+        max_length=255,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'question-input',
+                'placeholder': 'e.g. A weakness'
+            }
+        )
+    )
 
     class Meta:
         model=TemporaryQuestion
