c30637176451327f5e0956f820c63e783048a993
pedvel <pedvel@gmail.com>
2024-08-15 22:18:30 +0200

    room2 view

--- v1/urls.py
+++ v1/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'), # Create new room
-    path('<uuid:room_id>/<str:sender>/', views.sender, name='sender'), # Path for SENDER/Creator of room
-    path('<uuid:room_id>/share/<str:receiver>/', views.receiver, name='receiver'), # Path for RECEIVER of room
-    path('<uuid:room_id>/', views.room, name='room'), # View room
+    path('<uuid:room_id>/<str:name>/', views.room2, name='room2'),
+    path('answers/<str:name>/', views.answers, name='answers'),
+    
     path('s/<str:short_url>/', views.redirect_short_url, name='redirect_short_url'),
 ]
